45b28429b314889140d8eb9b3b665db2a41e5805
Andrew Njoo <adnjoo@gmail.com>
2023-11-29 21:44:11 -0800

    + improve post ui/ux

--- web/src/app/_components/create-post.tsx
+++ web/src/app/_components/create-post.tsx
@@ -27,32 +27,33 @@ export function CreatePost() {
         createPost.mutate({ name, notes, amount });
         // createPost.mutate({ name,  });
       }}
-      className="flex flex-col gap-2 max-w-xl justify-center mx-auto mt-4"
+      className="mx-auto mt-4 flex max-w-lg flex-col justify-center gap-2"
     >
+      <h3>Create New Subscription</h3>
       <input
         type="text"
         placeholder="Subscription Name"
         value={name}
         onChange={(e) => setName(e.target.value)}
-        className="w-full rounded-full px-4 py-2 text-black"
+        className="w-full rounded-md px-4 py-2 text-black"
       />
       <input
         type="text"
         placeholder="Notes"
         value={notes}
         onChange={(e) => setNotes(e.target.value)}
-        className="w-full rounded-full px-4 py-2 text-black"
+        className="w-full rounded-md px-4 py-2 text-black"
       />
       <input
         type="number"
         placeholder="Amount"
         value={amount}
         onChange={(e) => setAmount(Number(e.target.value))}
-        className="w-full rounded-full px-4 py-2 text-black"
+        className="w-full rounded-md px-4 py-2 text-black"
       />
       <button
         type="submit"
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
+        className="rounded-md bg-white/10 px-10 py-3 font-semibold transition hover:bg-white/20"
         disabled={createPost.isLoading}
       >
         {createPost.isLoading ? "Submitting..." : "Submit"}
