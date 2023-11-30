f8076795c8ddd89ec9f8edee762d5d3ef1da5201
Andrew Njoo <adnjoo@gmail.com>
2023-11-29 23:25:20 -0800

    twitter SEO

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import { getServerAuthSession } from "~/server/auth";
 import { CrudShowcase } from "./_components/CrudShowcase";
 import { LandingPageHero } from "./_components/organisms/LandingPageHero";
@@ -8,6 +10,15 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      {/* SEO */}
+      <Head>
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@adnjoo" />
+        <meta name="twitter:title" content="Subs Trackr" />
+        <meta name="twitter:description" content="Track your subscriptions." />
+        <meta name="twitter:image" content="/screenshot.png" />
+      </Head>
+
       <div className="container flex flex-col items-center gap-12 px-4 py-16 ">
         {!session ? <LandingPageHero /> : <CrudShowcase />}
       </div>
