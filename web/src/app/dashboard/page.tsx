b8ae2eb544b6015b18b6554abf3e76271788ae12
Andrew Njoo <adnjoo@gmail.com>
2023-12-01 04:57:25 -0800

    fix

--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -4,15 +4,17 @@ import { getServerAuthSession } from "~/server/auth";
 import { CrudShowcase } from "../_components";
 import { _PAGES } from "../lib/pages";
 
-export async function Dashboard() {
+export default async function Dashboard() {
   const session = await getServerAuthSession();
   return (
     <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center gap-12 px-4 py-32 ">
-        {!session ? <Link href={_PAGES.SIGN_IN}>Sign in to view dashboard</Link> : <CrudShowcase />}
+        {!session ? (
+          <Link href={_PAGES.SIGN_IN}>Sign in to view dashboard</Link>
+        ) : (
+          <CrudShowcase />
+        )}
       </div>
     </main>
   );
 }
-
-export default Dashboard;
