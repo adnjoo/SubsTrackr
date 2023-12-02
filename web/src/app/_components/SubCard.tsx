08113b43ffdb5694d93fd5b2f7fc7a3a94bf71a0
Andrew Njoo <adnjoo@gmail.com>
2023-12-02 13:50:33 -0800

    edit

--- web/src/app/_components/SubCard.tsx
+++ web/src/app/_components/SubCard.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import type { Subscription } from "@prisma/client";
-import { MdOutlineModeEditOutline as MdEdit, MdDelete } from "react-icons/md";
+import {
+  MdOutlineModeEditOutline as MdEdit,
+  MdDelete,
+  MdSave,
+} from "react-icons/md";
 
 import { reactApi } from "~/trpc/react";
 import { ExpenseIcons, SubscriptionIcons } from "../lib/icons";
@@ -14,14 +18,36 @@ export type SubCardProps = {
 
 export function SubCard({ sub }: SubCardProps) {
   const [isEditing, setIsEditing] = React.useState(false);
+  const [content, setContent] = React.useState<Subscription>(sub);
   const router = useRouter();
-  const deleteSub = reactApi.subscription.delete.useMutation({});
+  const deleteSub = reactApi.subscription.delete.useMutation({
+    onSuccess: () => {
+      router.refresh();
+    },
+  });
+  const upsertSub = reactApi.subscription.upsert.useMutation({
+    onSuccess: () => {
+      router.refresh();
+    },
+  });
+
+  const handleEdit = () => {
+    if (isEditing) {
+      setIsEditing(false);
+    } else {
+      setIsEditing(true);
+    }
+    // compare content with sub.content, if different upsert
+    if (content !== sub) {
+      // @ts-expect-error - TODO: Type 'string | null' is not assignable to type 'string'.
+      upsertSub.mutate({ ...content, id: sub.id });
+    }
+  };
 
   const handleDelete = async () => {
     // check before delete
     if (window.confirm("Are you sure you want to delete this subscription?")) {
       deleteSub.mutate(String(sub.id));
-      router.refresh();
     }
   };
 
@@ -42,20 +68,49 @@ export function SubCard({ sub }: SubCardProps) {
           height={40}
           unoptimized
         />
-      ) : <div className='h-10 w-10 ml-auto' />}
+      ) : (
+        <div className="ml-auto h-10 w-10" />
+      )}
 
       {/* Content */}
-      <p className="mb-2 text-lg font-bold">Name: {sub.name}</p>
-      <p className="mb-2">Amount: ${sub.amount}</p>
-      <p className="mb-2">Notes: {sub.notes}</p>
+      {isEditing ? (
+        <div>
+          <input
+            type="text"
+            value={content.name}
+            onChange={(e) => setContent({ ...content, name: e.target.value })}
+            className="mb-2 w-full rounded-md border border-gray-300 p-2 text-black"
+          />
+          <input
+            type="number"
+            value={content.amount}
+            onChange={(e) =>
+              setContent({ ...content, amount: Number(e.target.value) })
+            }
+            className="mb-2 w-full rounded-md border border-gray-300 p-2 text-black"
+          />
+          <input
+            type="text"
+            value={content.notes!}
+            onChange={(e) => setContent({ ...content, notes: e.target.value })}
+            className="mb-2 w-full rounded-md border border-gray-300 p-2 text-black"
+          />
+        </div>
+      ) : (
+        <div>
+          <p className="mb-2 text-lg font-bold">Name: {sub.name}</p>
+          <p className="mb-2">Amount: ${sub.amount}</p>
+          <p className="mb-2">Notes: {sub.notes}</p>
+        </div>
+      )}
 
+      {/* Actions */}
       <div>
-        {/* Edit button */}
         <button
-          onClick={() => setIsEditing(!isEditing)}
+          onClick={handleEdit}
           className="mr-2 rounded-md bg-blue-500 px-2 py-1 text-white transition hover:bg-blue-600 focus:outline-none"
         >
-          <MdEdit />
+          {isEditing ? <MdSave /> : <MdEdit />}
         </button>
         <button
           onClick={handleDelete}
